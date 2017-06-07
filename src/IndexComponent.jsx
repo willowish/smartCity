714e2e21fa6d6d7c22fa78583f33bf026dcdb57c
willowish <mateusz.wierzbiicki@gmail.com>
2017-06-07 22:02:12 +0200

    init

---
+++ src/IndexComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class IndexComponent extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+let webpack = require('webpack');
+let path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, './src'),
+    target: 'web',
+    entry: {
+        index: ['./index.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$|\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['es2015', 'react', 'stage-0'],
+                        plugins: ['react-html-attrs', 'transform-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader?sourceMap', 'postcss-loader']
+            }
+        ]
+    },
+    plugins: {},
+    resolve: {
+        extensions: ['.js', '.json', '.jsx']
+    },
+    output: {
+        path: __dirname + "/src/",
+        filename: 'index.min.js'
+        // filename: util.format('[name].%s.js', pkg.version)
+    }
+};
