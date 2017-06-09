c697b4d9f61195956a72447ec26a3c145d12b29d
mawierzb <mateusz.wierzbicki@nokia.com>
2017-06-09 18:26:55 +0200

    MERGE?

--- frontend/src/indexApp.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Index from './IndexComponent.jsx';
-const app = document.getElementById('app');
-
-ReactDOM.render(<Index />, app);
